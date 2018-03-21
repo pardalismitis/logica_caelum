7f50ed65978107a3ae65755d448b3a9bad9b90bd
Fernando Lima <pardalismitis@users.noreply.github.com>
2018-03-21 14:58:07 -0300

    Update biblioteca_funcoes.js

--- biblioteca_funcoes.js
+++ biblioteca_funcoes.js
@@ -1,12 +1,18 @@
-//CONVERTE VALOR INSERIDO PELO USUÁRIO (STRING) EM MENSAGEM DE POP-UP PARA NÚMERO (FLOAT)
+//CONVERTE VALOR INSERIDO PELO USUÁRIO (STRING) EM MENSAGEM DE POP-UP PARA NÚMERO DECIMAL (FLOAT)
 //LEMBRAR DE MODIFICAR raw.githubusercontent POR rawgit
 
 function valorInseridoPeloUsuario(mensagemPopUp){
-        return parseFloat(prompt(mensagemPopUp).replace(",",".").replace("o","0"));
+    return parseFloat(prompt(mensagemPopUp).replace(",",".").replace("o","0"));
 }
 
 //CONVERTE VALOR CALCULADO DE NÚMERO (FLOAT) PARA TEXTO (STRING) PARA EXIBIÇÃO NA TELA
 
 function pontoParaVirgula(valor){
-    return valor.toFixed(2).replace(".",",");
+return valor.toFixed(2).replace(".",",");
+}
+
+//CONVERTE VALOR INSERIDO PELO USUÁRIO (STRING) EM MENSAGEM DE POP-UP PARA NÚMERO INTEIRO (INT)
+
+function valorInseridoPeloUsuarioInt(mensagemPopUp){
+    return parseFloat(prompt(mensagemPopUp).replace(",",".").replace("o","0"));
 }
