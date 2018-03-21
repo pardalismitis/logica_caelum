0b0de91af4770844a0600726f0c0601f70f89793
Fernando Lima <pardalismitis@users.noreply.github.com>
2018-03-21 09:30:38 -0300

    Update biblioteca_funcoes.js

--- biblioteca_funcoes.js
+++ biblioteca_funcoes.js
@@ -1,7 +1,10 @@
+//CONVERTE VALOR INSERIDO PELO USUÁRIO (STRING) EM MENSAGEM DE POP-UP PARA NÚMERO (FLOAT)
+
 function valorInseridoPeloUsuario(){
         return parseFloat(prompt(mensagemPopUp).replace(",",".").replace("o","0"));
 }
 
+//CONVERTE VALOR CALCULADO DE NÚMERO (FLOAT) PARA TEXTO (STRING) PARA EXIBIÇÃO NA TELA
 function pontoParaVirgula(){
     return valor.toFixed(2).replace(".",",");
 }
